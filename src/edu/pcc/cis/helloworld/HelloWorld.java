83deafda0e5b76b71c3e8c32cfd0e7d0bc687cd4
Jeff Butler <Jbutler7@gmail.com>
2020-04-24 07:56:02 -0700

    Added a message for my instructor

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,6 +4,8 @@ import java.util.Date;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Pochin, today's date and time is: ");
         System.out.println(new Date());
     }
+
 }
